014c6c993d24bf69a2db8b72168988fa062dcbfb
Jing.Dai <daijing_emily@163.com>
2017-10-07 15:35:12 +0800

    modify tests

--- checkin_app/tests.py
+++ checkin_app/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from checkin_app.models import User
 
 class TagTests(APITestCase):
     def test(self):
@@ -27,33 +26,9 @@ class TagTests(APITestCase):
         response = self.client.get('/checkin/tags/1/')
         self.assertEqual(response.status_code, 404)
 
-class RuleTests(APITestCase):
-    def test(self):
-        data = {'duration': 20}
-        response = self.client.post('/checkin/rules/', data, format='json')
-        self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/checkin/rules/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data[0]['duration'], 20)
-
-        response = self.client.get('/checkin/rules/1/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['duration'], 20)
-
-        data = {'duration': 30}
-        response = self.client.put('/checkin/rules/1/', data, format='json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['duration'], 30)
-
-        response = self.client.delete('/checkin/rules/1/')
-        self.assertEqual(response.status_code, 204)
-        response = self.client.get('/checkin/rules/1/')
-        self.assertEqual(response.status_code, 404)
-
 class ProjectTests(APITestCase):
     def test(self):
-        data = {"tag_id": 1, "rule_id": 1, "notice": "haha"}
+        data = {"tag_id": 1, "notice": "haha", "frequency":1}
         response = self.client.post('/checkin/projects/', data, format='json')
         self.assertEqual(response.status_code, 200)
 
